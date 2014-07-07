543b4e58a5239cb6c4b2b4d314358bda7d94f9b0
Leroy Campbell <leroy@artisonian.com>
2014-07-07 14:59:51 -0400

    replace fs.watch with chokidar
    Fixes #1 and #2

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-var fs = require('fs');
+var domain = require('domain');
 var path = require('path');
 var spawn = require('child_process').spawn;
 
+var chokidar = require('chokidar');
 var Metalsmith = require('metalsmith');
 var templates = require('metalsmith-templates');
 var sass = require('metalsmith-sass');
 
 var BROWSER_SYNC_BIN = path.resolve('./node_modules/.bin/browser-sync');
+var browserSync;
+
+process.on('exit', exit);
 
 var assets = Metalsmith(__dirname)
+  .clean(false)
   .use(templates({
     engine: 'nunjucks',
     inPlace: true
@@ -17,21 +22,31 @@ var assets = Metalsmith(__dirname)
     outputStyle: 'compact'
   }));
 
-assets.build(function (err, res) {
-  if (err) { console.error(err); }
+var d = domain.create();
 
-  fs.watch(path.join(__dirname, 'src'), watch);
-  fs.watch(path.join(__dirname, 'templates'), watch);
+d.on('error', exit);
 
-  spawn(BROWSER_SYNC_BIN, ['start', '--files', 'build/*', '--server', 'build'], {
-    stdio: ['ignore', process.stdout, process.stderr]
+d.run(function () {
+  assets.build(function (err, res) {
+    if (err) { console.error(err); }
+
+    chokidar.watch(path.join(__dirname, 'src')).on('all', watch);
+    chokidar.watch(path.join(__dirname, 'templates')).on('all', watch);
+
+    browserSync = spawn(BROWSER_SYNC_BIN, ['start', '--files', 'build/*', '--server', 'build'], {
+      stdio: ['ignore', process.stdout, process.stderr]
+    });
   });
 });
 
-function watch (event, filename) {
-  if (filename) {
-    assets.build(function (err, res) {
-      if (err) { return console.error(err); }
-    });
-  }
+function watch (event, path) {
+  console.log('detected changes:', path);
+  assets.build(function (err, res) {
+    if (err) { return console.error(err); }
+  });
+}
+
+function exit (err) {
+  if (err) { console.error(err.stack ? err.stack : new Error('Unknown error')); }
+  if (browserSync) { browserSync.kill(); }
 }
